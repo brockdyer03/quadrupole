dcfbe17d5b29956f392c57103eeaf8406ab66e7c
brockdyer03 <brockdyer03@gmail.com>
2026-01-11 16:33:25 -0500

    Add `.cube` test, and use `numpy.testing` for array equality tests, also update Numpy version in `uv.lock` to 2.4.1
    Signed-off-by: brockdyer03 <brockdyer03@gmail.com>

--- tests/test_geometry.py
+++ tests/test_geometry.py
@@ -68,8 +68,8 @@ def test_geometry():
     )
 
     assert(geometry.atoms == atoms)
-    assert(np.all(geometry.get_coords() == xyzs))
     assert(geometry.get_elements() == elements)
+    np.testing.assert_array_equal(geometry.get_coords(), xyzs)
 
     ref_eigenvalues = np.array(
         [1261.1061354199865, -1.1368683772161603e-13, 1261.1061354199867],
@@ -78,16 +78,19 @@ def test_geometry():
 
     ref_eigenvectors = np.array(
         [
-            [ 0.81649658, -0.57735027,  0.02797921],
-            [-0.40824829, -0.57735027, -0.72068110],
-            [-0.40824829, -0.57735027,  0.69270189],
+            [ 0.816496580928, -0.57735026919,  0.027979205835],
+            [-0.408248290464, -0.57735026919, -0.720681100695],
+            [-0.408248290464, -0.57735026919,  0.692701894860],
         ], dtype=np.float64
     )
 
     eigenvalues, eigenvectors = geometry.calc_principal_moments()
 
-    assert(np.all(eigenvalues.round(8) == ref_eigenvalues.round(8)))
-    assert(np.all(eigenvectors.round(8) == ref_eigenvectors.round(8)))
+    print(eigenvalues)
+    print(eigenvectors)
+
+    np.testing.assert_allclose(eigenvalues,  ref_eigenvalues,  rtol=1e-8)
+    np.testing.assert_allclose(eigenvectors, ref_eigenvectors, rtol=1e-8)
 
 
 def test_geometry_from_list():
@@ -139,7 +142,7 @@ def test_geometry_from_xyz(tmp_path):
     geometry = Geometry.from_xyz(xyz_path)
 
     assert(geometry.get_elements() == elements)
-    assert(np.all(geometry.get_coords() == xyzs))
+    np.testing.assert_array_equal(geometry.get_coords(), xyzs)
 
 
 def test_geometry_from_xsf(tmp_path):
@@ -186,8 +189,8 @@ def test_geometry_from_xsf(tmp_path):
     geometry = Geometry.from_xsf(xsf_path)
 
     assert(geometry.get_elements() == elements)
-    assert(np.all(geometry.get_coords() == xyzs))
-    assert(np.all(geometry.lat_vec == lat_vec))
+    np.testing.assert_array_equal(geometry.get_coords(), xyzs)
+    np.testing.assert_array_equal(geometry.lat_vec, lat_vec)
     assert(geometry.alat == alat)
 
 
@@ -204,12 +207,6 @@ def test_geometry_from_orca():
         [5.738252, 3.930645, 3.432457],
     ], dtype=np.float64)
 
-    input_xyzs = np.array([
-        [5.736637, 3.296256, 2.732843],
-        [4.932128, 3.398823, 2.238126],
-        [5.754947, 3.927980, 3.442132],
-    ], dtype=np.float64)
-
     orca_output_path = Path(
         __file__ + "../../../notebook/example_outputs/water_random_rotation.out"
     ).resolve()
@@ -217,5 +214,58 @@ def test_geometry_from_orca():
     geometry = Geometry.from_orca(orca_output_path)
 
     assert(geometry.get_elements() == elements)
-    assert(np.all(geometry.get_coords() == final_xyzs))
-    assert(np.all(geometry.get_coords() != input_xyzs))
+    np.testing.assert_array_equal(geometry.get_coords(), final_xyzs)
+
+
+def test_geometry_from_cube(tmp_path):
+    elements = [
+        Element.Oxygen,
+        Element.Hydrogen,
+        Element.Hydrogen,
+    ]
+
+    xyzs = np.array([
+        [12.75798576, 13.05368522, 12.76021095],
+        [13.52042164, 12.47252958, 12.76021095],
+        [12.00000026, 12.46673668, 12.76021095],
+    ], dtype=np.float64)
+
+    lat_vec = np.array([
+        [25.52039649,  0.00000000,  0.00000000],
+        [ 0.00000000, 25.52039649,  0.00000000],
+        [ 0.00000000,  0.00000000, 25.52039649],
+    ], dtype=np.float64)
+
+    alat = 25.52039649
+
+    cube = (
+        " Cubefile created from PWScf calculation\n"
+        "Contains the selected quantity on a FFT grid\n"
+        "    3    0.000000    0.000000    0.000000\n"
+        "  320    0.150708    0.000000    0.000000\n"
+        "  320    0.000000    0.150708    0.000000\n"
+        "  320    0.000000    0.000000    0.150708\n"
+        "    8    8.000000   24.109099   24.667890   24.113304\n"
+        "    1    1.000000   25.549894   23.569665   24.113304\n"
+        "    1    1.000000   22.676714   23.558718   24.113304\n"
+        " -0.22991E-06  0.15171E-06  0.49928E-07 -0.95286E-07 -0.28930E-07  0.81718E-08\n"
+        "  0.17149E-06 -0.27311E-06  0.11460E-06  0.74206E-07 -0.12380E-06 -0.50463E-07\n"
+        " -0.56805E-07  0.25518E-06 -0.13170E-06 -0.15147E-07  0.13074E-06  0.23688E-07\n"
+        " -0.11941E-06  0.43801E-07  0.69832E-07 -0.10074E-06  0.95414E-07 -0.43724E-07\n"
+        "  0.16973E-08 -0.29553E-07  0.98935E-07 -0.51880E-07 -0.10265E-06  0.19040E-06\n"
+        " -0.46587E-07 -0.49705E-07  0.41958E-07  0.68452E-09 -0.13268E-06  0.44203E-07\n"
+    )
+
+    temp_dir = tmp_path / Path("test_files")
+    temp_dir.mkdir()
+
+    cube_path = temp_dir / Path("test.cube")
+
+    cube_path.write_text(cube, encoding="utf-8")
+
+    geometry = Geometry.from_cube(cube_path)
+
+    assert(geometry.get_elements() == elements)
+    np.testing.assert_allclose(geometry.get_coords(), xyzs, rtol=1e-8)
+    np.testing.assert_allclose(geometry.lat_vec, lat_vec, rtol=1e-7)
+    np.testing.assert_almost_equal(geometry.alat,  alat, decimal=8)
